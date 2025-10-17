eccb5283bddfa75d0f8dd3e88eed7fa56ccfc0f8
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-10-17 09:16:10 +0700

    belajar fundamental main debugging

---
+++ lib/doc-about.dart
@@ -0,0 +1,62 @@
+
+/**
+ * penjelasan
+
+Struktur flutter terdiri dari
+1. .dart_tool
+2. .idea
+3. android
+ => untuk konfigurasi android serta
+ => libary android
+
+4. build
+ => hasil compile
+
+5. ios
+ => untuk konfigurasi ios serta
+ => libary android
+
+6. lib
+ => tempat file dart berada yang nantinya
+ => sering digunakan
+
+7. Linux
+ => untuk konfigurasi linux serta
+ => libary android
+
+8. macos
+ => untuk konfigurasi macos
+
+9. test
+ => tempat file untuk testing aplikasi
+
+10. web
+ => untuk konfigurasi web
+
+11. windows
+ => untuk konfigurasi windows
+
+12. pubspec.yaml
+ => untuk konfigurasi aplikasi
+ => menambahkan library
+ => menambahkan asset (gambar , font dll)
+
+diterminal perintah flutter pub get
+ => unutk mensycron libray yang ada di pubspec.yaml
+
+kegunnan perintah "flutter run"
+ => melanjatkan aplikasi
+
+kegunaan perintah "flutter clean"
+ => membersihkan file build
+
+library dari flutter bisa dilihat di pub.dev
+
+di dalam dart ada 2 statement
+1. statement widget 
+ => tdak ada perubahan data
+2. stateful widget
+ => ada perubahan data
+
+
+ */
