c71be0c73c4da65eb874601a6d193304b00522c8
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-10-23 13:53:08 +0700

    belajar container

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({super.key});
+class profilecard extends StatelessWidget {
+  const profilecard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +10,47 @@ class ProfileCard extends StatelessWidget {
         backgroundColor: Colors.redAccent,
         title: const Text(
           'My Profile',
-          style: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Ucup Garnaco',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            Row(
-              children: <Widget>[
-                
-              ],
-            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(28),
+                color: Colors.amberAccent,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 50),
+                    child: Text(
+                      'NAME',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Ucep Guerero',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
