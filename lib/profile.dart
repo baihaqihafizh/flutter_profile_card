15646fe9619fb28bee039fb8bfc73b3b10d3cddd
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-10-24 09:42:49 +0700

    belajar row

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class profilecard extends StatelessWidget {
-  const profilecard({super.key});
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +9,7 @@ class profilecard extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
         title: const Text(
-          'My Profile',
+          'My profile',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
@@ -18,37 +18,85 @@ class profilecard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 100,
+              width: MediaQuery.of(context).size.width * 0.8 ,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(28),
-                color: Colors.amberAccent,
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.amber,
                 boxShadow: [
                   BoxShadow(
                     color: Colors.redAccent,
                     spreadRadius: 5.0,
                     blurRadius: 7.0,
                     offset: Offset(0, 3),
-                  ),
-                ],
+                  )
+                ]
               ),
+              height: 300,
               child: Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(top: 30, bottom: 50),
-                    child: Text(
-                      'NAME',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      ),
+                    child: Text('NAME',
+                    style:  TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),),
+                  ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  Text(
+                    'Baihaqi Hafizh A.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Ucep Guerero',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.redAccent,
+                                  borderRadius: BorderRadius.circular(
+                                    15,
+                                    ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text('Address'),
+                                ),
+                                ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.redAccent,
+                                    borderRadius: BorderRadius.circular(
+                                      15
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('Tempat Tanggal lahir'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                    ],),
                 ],
               ),
             ),
